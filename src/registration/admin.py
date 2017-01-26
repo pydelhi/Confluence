1880cf0afbc055d3962aea1b656fc09540cc6318
Amit Kumar <aktech@users.noreply.github.com>
2017-01-27 00:03:40 +0530

    Fixes registration the custom User model by extending UserAdmin (#9)

--- src/registration/admin.py
+++ src/registration/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
 # Register your models here.
 
-class UserAdmin(admin.ModelAdmin):
+
+class CustomUserAdmin(UserAdmin):
     list_display = [field.name for field in User._meta.fields]
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
